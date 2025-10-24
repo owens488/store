88af37ee3ff9cdfd6eb44edcc4711a3faf1ea3be
owens488 <yaroslavtuka2007@gmail.com>
2025-10-24 21:49:04 +0200

    Реализовать поиск по ISBN и автору

---
+++ domain/Store/BookService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class BookService
+    {
+        private readonly IBookRepository bookRepository;
+
+        public BookService(IBookRepository bookService)
+        {
+            this.bookRepository = bookService;
+        }
+
+        public Book[] GetAllByQuery(string query)
+        {
+            if (Book.IsIsbn(query))
+                return bookRepository.GetAllByIsbn(query);
+
+            return bookRepository.GetAllByTitleOrAuthor(query);
+        }
+    }
+}
