92d807f85efe87b218e2e2f155ad1c3ba4dcbf0a
owens488 <yaroslavtuka2007@gmail.com>
2025-10-29 23:39:20 +0200

    Реализовать оплату наличными

---
+++ domain/Store/OrderDelivery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class OrderDelivery
+    {
+        public string UniqueCode { get; }
+
+        public string Description { get; }
+
+        public decimal Amount { get; }
+
+        public IReadOnlyDictionary<string, string> Parameters { get; }
+
+        public OrderDelivery(string uniqueCode,
+                             string description,
+                             decimal amount,
+                             IReadOnlyDictionary<string, string> parameters)
+        {
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentNullException(nameof(uniqueCode));
+
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentNullException(nameof(description));
+
+            if (parameters == null)
+                throw new ArgumentNullException(nameof(parameters));
+
+            UniqueCode = uniqueCode;
+            Description = description;
+            Amount = amount;
+            Parameters = parameters;
+        }
+    }
+}
