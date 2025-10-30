5b42105eeaebb2a26d2d3eb76236eb738a483764
owens488 <yaroslavtuka2007@gmail.com>
2025-10-30 18:55:00 +0200

    Реализовать оплату банкосвкой картой

---
+++ plugins/Store.YandexKassa/YandexKassaPaymentService.cs
@@ -0,0 +1,30 @@
+﻿using Store.Contractors;
+using System.Collections.Generic;
+using Store.Web.Contractors;
+
+namespace Store.YandexKassa
+{
+    public class YandexKassaPaymentService : IPaymentService, IWebContractorService
+    {
+        public string UniqueCode => "YandexKassa";
+
+        public string Title => "Оплата банковской картой";
+
+        public string GetUri => "/YandexKassa/";
+
+        public Form CreateForm(Order order)
+        {
+            return new Form(UniqueCode, order.Id, 1, true, new Field[0]);
+        }
+
+        public OrderPayment GetPayment(Form form)
+        {
+            return new OrderPayment(UniqueCode, "Оплата картой", new Dictionary<string, string>());
+        }
+
+        public Form MoveNextForm(int orderId, int step, IReadOnlyDictionary<string, string> value)
+        {
+            return new Form(UniqueCode, orderId, 2, true, new Field[0]);
+        }
+    }
+}
